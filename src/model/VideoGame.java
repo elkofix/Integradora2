40bef9df25d8b6a3a12a24cd2dac9c02cd60814a
elkofix <holasoygermamwow@gmail.com>
2022-09-29 21:55:46 -0500

    Creating the main class and part of the VideoGame class

---
+++ src/model/VideoGame.java
@@ -0,0 +1,13 @@
+package model;
+
+public class VideoGame {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public VideoGame(String name){
+        this.name = name;
+    }
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,64 @@
+package ui;
+
+import model.VideoGame;
+
+import java.lang.StackWalker.Option;
+import java.util.Scanner;
+
+public class Main{
+
+    private VideoGame videoGame;
+    private Scanner reader;
+    public static void main(String[] args) {
+        Main main = new Main();
+        int option = 0;
+        do{
+          option = main.getOptionShowMenu();
+          main.executeOption(option);
+        }while(option!=0);
+    }
+
+    public Main(){
+       videoGame = new VideoGame("Nico's Adventure");
+       reader = new Scanner(System.in);
+    }
+    public int getOptionShowMenu(){ 
+        int option = 0;
+        System.out.println("<<Bienvenido a "+videoGame.getName()+" >> \n"+
+        "1. Crear jugador \n"+
+        "2. Registra enemigo a un nivel \n"+´
+        "3. Modificar el puntaje de un jugador \n"+
+        "4. Subir de nivel a un jugador");
+        option = reader.nextInt();
+
+        return option;
+    }
+    public void executeOption(int option){ 
+     
+        switch(option){
+            case 1:
+                
+            break;
+
+            case 2: 
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+
+            break;
+
+            default:
+
+            break;
+        }
+
+    }
+
+
+
+}//end of class
