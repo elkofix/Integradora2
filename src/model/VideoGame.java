823191ae84c85e3664076a68ade8df12e8cd1b69
elkofix <holasoygermamwow@gmail.com>
2022-10-02 21:11:28 -0500

    Organizing methods and adding requirements documentation

--- src/model/VideoGame.java
+++ src/model/VideoGame.java
@@ -23,6 +23,250 @@ public class VideoGame {
 		createRandomEnemies();
 		createRandomTreasures();
     }
+
+	public String addPlayer(Player newPlayer) {
+		int pos = searchPlayerbyNick(newPlayer.getNickname());
+		String msj = "";
+
+		if (pos==-1){
+			pos = playerHasEmptyPos();
+			if(pos!=-1){
+				players[pos] = newPlayer;
+				msj = "Jugador agregado";
+			}else{
+				msj = "Nuestra capacidad de jugadores ya esta en su limite";
+			}
+			
+		}else{
+			msj = "El nickname ya exise, usa otro";
+		}
+		return msj;
+	}
+
+	/**
+	 * 
+	 * @param level
+	 * @param Enemy
+	 */
+	public String addEnemyToLevel(Enemy enemy, String levelid, int type) {
+		int pos = searchLevelById(levelid);
+		boolean isRepeated;
+        int enemyPos;
+        String msj ="";
+        if (pos==-1){
+            msj = "No se encontro el nivel";
+        }else{
+			isRepeated = canvas.getLevels()[pos].isEnemyRepeated(enemy.getName());
+			if (!isRepeated){
+				enemyPos = canvas.getLevels()[pos].enemyHasEmptyPos();
+            	if(enemyPos ==-1){
+                	msj = "La capacidad de enemigos esta en su limite";
+           		}else{
+					enemy.setType(giveType(type));
+                	canvas.getLevels()[pos].getEnemies()[enemyPos]=enemy;
+					canvas.getLevels()[pos].calculateDifficulty();
+                	msj = "Enemigo agregado";
+
+            	}
+			}else{
+				msj = "El enemigo ya existe en este nivel";
+			}
+            
+        }
+        return msj;
+	}
+
+	public String addTreasureToLevel(String levelid, Treasure newTreasure) {
+		int pos = searchLevelById(levelid);
+		boolean isRepeated;
+        int treasurePos;
+        String msj ="";
+        if (pos==-1){
+            msj = "No se encontro el nivel";
+        }else{
+			isRepeated = canvas.getLevels()[pos].isTreasureRepeated(newTreasure.getName());
+			if (!isRepeated){
+				treasurePos = canvas.getLevels()[pos].TreasureHasEmptyPos();
+            	if(treasurePos ==-1){
+                	msj = "La capacidad de enemigos esta en su limite";
+           		}else{
+                	canvas.getLevels()[pos].getTreasures()[treasurePos]=newTreasure;
+					canvas.getLevels()[pos].calculateDifficulty();
+                	msj = "Tesoro agregado";
+
+            	}
+			}else{
+				msj = "El tesoro ya existe en este nivel";
+			}
+            
+        }
+        return msj;
+	}
+
+	public String changePlayerScore(String nick, int newScore) {
+		String msj  = "";
+		int pos = searchPlayerbyNick(nick);
+		if(pos==-1){
+			msj = "No se encontro al jugador";
+		}else{
+			players[pos].setScore(newScore);
+			msj = "Puntaje actualizado";
+		}
+		return msj;
+	}
+
+	public String upgradePlayer(String nick) {
+		String msj = "";
+		int playerScore = 0;
+		int pos = searchPlayerbyNick(nick);
+		if (pos != -1){
+			playerScore = players[pos].getScore();
+			if(players[pos].getCurrentlevel().getReachPoints()>playerScore){
+				msj = "Este jugador no puede avanzar de nivel aun";
+			}else{
+				for(int i = 0; i<canvas.getLevels().length; i++){
+					if(playerScore>canvas.getLevels()[i].getReachPoints()){
+						players[pos].setCurrentlevel(canvas.getLevels()[i+1]);
+						msj = "Felicidades el jugador avanzo hasta el nivel "+canvas.getLevels()[i+1].getId();
+					}
+				}
+				
+			}
+		}else{
+			msj = "No se encontro al usuario";
+		}
+		return msj;
+	}
+
+	public String showTreasuresAndEnemys(String id){
+		int pos = searchLevelById(id);
+		String msj = "";
+		if (pos !=-1){
+			for (int i = 0; i<canvas.getLevels()[pos].getTreasures().length; i++){
+				if(canvas.getLevels()[pos].getTreasures()[i]!=null){
+					msj += canvas.getLevels()[pos].getTreasures()[i].getName()+" Diamantes: "+canvas.getLevels()[pos].getTreasures()[i].getValue()+", \n";
+				}	
+			}
+			for (int i = 0; i<canvas.getLevels()[pos].getEnemies().length; i++){
+				if(canvas.getLevels()[pos].getEnemies()[i]!=null){
+					msj += canvas.getLevels()[pos].getEnemies()[i].getName()+" Ataque: "+canvas.getLevels()[pos].getEnemies()[i].getAttack()+" Botin "+canvas.getLevels()[pos].getEnemies()[i].getLoot()+", \n";
+				}	
+			}
+		}else{
+			msj ="No se encontro el nivel";
+		}
+		return msj;
+	}
+
+	public String getAllTreasures(){
+		int total = 0;
+		String msj ="";
+		for (int i = 0; i<canvas.getLevels().length; i++){
+			total+=canvas.getLevels()[i].countTresuresValue();
+		}
+		return msj = "En total de los niveles hay "+total+" Diamantes";
+	}
+
+	
+	public String getAllEnemiesType(int type){
+		Type toSearch = giveType(type);
+		int total = 0;
+		String msj;
+		for (int i = 0; i<canvas.getLevels().length; i++){
+			total +=canvas.getLevels()[i].countEnemiesByType(toSearch);
+			
+		}
+		return msj = "En total de los niveles hay "+total+" "+toSearch+"(s)";
+	}
+
+	public String getModeTreasure() {
+		String msj = "";
+        ArrayList<int[]> treasureValues = new ArrayList<int[]>();
+        int value = 0;
+		boolean isAdded;
+        int[] values = new int[]{0, 0};
+		treasureValues.add(values);
+		for(int i = 0; i<canvas.getLevels().length; i++){
+			for(int j = 0; j<canvas.getLevels()[0].getTreasures().length; j++){
+				if(canvas.getLevels()[i].getTreasures()[j]!=null){
+					values = new int[]{canvas.getLevels()[i].getTreasures()[j].getValue(), 1};
+					isAdded = true;
+					for(int k = 0; k<treasureValues.size(); k++){
+						if(treasureValues.get(k)[0]==values[0]){
+							treasureValues.get(k)[1]+=1;
+							isAdded = false;
+						}
+					}
+					if (isAdded == true){
+						treasureValues.add(values);	
+					}
+					
+				}
+			}
+		}
+			int[] max = treasureValues.get(0);
+			int[] min;
+			for(int i = 1; i<treasureValues.size(); i++){
+				min = treasureValues.get(i)	;
+				if(min[1]>max[1]){
+					max = min;
+				}
+			}
+        msj ="El tesoro que mas se repite en todos los niveles son los que contienen "+max[0]+" Diamantes \n"+
+		"Total: "+max[1];
+        return msj;
+	}
+
+	public String getMaxEnemy(){
+		String msj="No se encontraron enemigos";
+		int pos = -1;
+		int enemyPos= -1;
+		int max = 0;
+		int min = 0;
+		for(int i = 0; i<canvas.getLevels().length; i++){
+			for(int j = 0; j<canvas.getLevels()[0].getEnemies().length; j++){
+				
+				if (canvas.getLevels()[i].getEnemies()[j]!=null){
+					min = canvas.getLevels()[i].getEnemies()[j].getLoot();
+					if(min > max){
+						max = min;
+						pos = i;
+						enemyPos = j;
+					}
+				}
+			}
+		}
+		if (enemyPos != -1 && pos != -1){
+			msj = "El enemigo que mas puntos otorga es un "+canvas.getLevels()[pos].getEnemies()[enemyPos].getType()+" en el nivel "+(pos+1)+"\n"+
+			"Puntos en total: "+canvas.getLevels()[pos].getEnemies()[enemyPos].getLoot();
+		}
+		return msj;
+	}
+	
+	public String getALLConsonants(){
+		String msj = "";
+		int contador = 0;
+		String enemyName = "";
+		String consonantes ="bcdfghjklmnpqrstvwxyz"; 
+		for(int i = 0; i<canvas.getLevels().length; i++){
+			for(int j = 0; j<canvas.getLevels()[0].getEnemies().length; j++){
+				if (canvas.getLevels()[i].getEnemies()[j]!=null){
+					enemyName = canvas.getLevels()[i].getEnemies()[j].getName();
+					for(int k = 0; k<consonantes.length(); k++){
+						for(int l= 0; l<enemyName.length(); l++){
+							if(enemyName.charAt(l)==consonantes.charAt(k)){
+								contador++;
+							}
+						
+						}
+					}
+				}
+			}
+		
+		
+		}
+		return msj ="En el juego los enemigos tienen un total de "+contador+" consonantes en sus nombres";
+	}
 	public void createRandomEnemies(){
 		
 		String[] dryNames = new String[]{"Mara_Grand","Bossk_Natasi","C-3PO_Cade","Han_Zam","Revan_PROXY","Grand_Kir","Watto_Bossk","Gilad_Jaina","Zayne_Watto","IG_Jabba","Mace_Kyle","Lando_Jarael","Grand_Bib","Anakin_Cad","Boba_Aayla","Shaak_Cade","Grand_Senator","Chewbacca_C-3PO","Senator_Nom","Galen_Ahsoka","Mission_Zam","Lando_Chewbacca","Qui-Gon_Wedge","Visas_R2-D2","Captain_Admiral"};
@@ -64,39 +308,8 @@ public class VideoGame {
 
 		
 	}
-    public String upgradePlayer(String nick) {
-		String msj = "";
-		int playerScore = 0;
-		int pos = searchPlayerbyNick(nick);
-		if (pos != -1){
-			playerScore = players[pos].getScore();
-			if(players[pos].getCurrentlevel().getReachPoints()>playerScore){
-				msj = "Este jugador no puede avanzar de nivel aun";
-			}else{
-				for(int i = 0; i<canvas.getLevels().length; i++){
-					if(playerScore>canvas.getLevels()[i].getReachPoints()){
-						players[pos].setCurrentlevel(canvas.getLevels()[i+1]);
-						msj = "Felicidades el jugador avanzo hasta el nivel "+canvas.getLevels()[i+1].getId();
-					}
-				}
-				
-			}
-		}else{
-			msj = "No se encontro al usuario";
-		}
-		return msj;
-	}
-	public String changePlayerScore(String nick, int newScore) {
-		String msj  = "";
-		int pos = searchPlayerbyNick(nick);
-		if(pos==-1){
-			msj = "No se encontro al jugador";
-		}else{
-			players[pos].setScore(newScore);
-			msj = "Puntaje actualizado";
-		}
-		return msj;
-	}
+
+
 
 	/**
 	 * 
@@ -107,108 +320,17 @@ public class VideoGame {
 		throw new UnsupportedOperationException();
 	}
 
-	public String addPlayer(Player newPlayer) {
-		int pos = searchPlayerbyNick(newPlayer.getNickname());
-		String msj = "";
 
-		if (pos==-1){
-			pos = playerHasEmptyPos();
-			if(pos!=-1){
-				players[pos] = newPlayer;
-				msj = "Jugador agregado";
-			}else{
-				msj = "Nuestra capacidad de jugadores ya esta en su limite";
-			}
-			
-		}else{
-			msj = "El nickname ya exise, usa otro";
-		}
-		return msj;
-	}
-	public String showTreasuresAndEnemys(String id){
-		int pos = searchLevelById(id);
-		String msj = "";
-		if (pos !=-1){
-			for (int i = 0; i<canvas.getLevels()[pos].getTreasures().length; i++){
-				if(canvas.getLevels()[pos].getTreasures()[i]!=null){
-					msj += canvas.getLevels()[pos].getTreasures()[i].getName()+" Diamantes: "+canvas.getLevels()[pos].getTreasures()[i].getValue()+", \n";
-				}	
-			}
-			for (int i = 0; i<canvas.getLevels()[pos].getEnemies().length; i++){
-				if(canvas.getLevels()[pos].getEnemies()[i]!=null){
-					msj += canvas.getLevels()[pos].getEnemies()[i].getName()+" Ataque: "+canvas.getLevels()[pos].getEnemies()[i].getAttack()+" Botin "+canvas.getLevels()[pos].getEnemies()[i].getLoot()+", \n";
-				}	
-			}
-		}else{
-			msj ="No se encontro el nivel";
-		}
-		return msj;
-	}
 
-	/**
-	 * 
-	 * @param level
-	 * @param Enemy
-	 */
-	public String addEnemyToLevel(Enemy enemy, String levelid, int type) {
-		int pos = searchLevelById(levelid);
-		boolean isRepeated;
-        int enemyPos;
-        String msj ="";
-        if (pos==-1){
-            msj = "No se encontro el nivel";
-        }else{
-			isRepeated = canvas.getLevels()[pos].isEnemyRepeated(enemy.getName());
-			if (!isRepeated){
-				enemyPos = canvas.getLevels()[pos].enemyHasEmptyPos();
-            	if(enemyPos ==-1){
-                	msj = "La capacidad de enemigos esta en su limite";
-           		}else{
-					enemy.setType(giveType(type));
-                	canvas.getLevels()[pos].getEnemies()[enemyPos]=enemy;
-					canvas.getLevels()[pos].calculateDifficulty();
-                	msj = "Enemigo agregado";
 
-            	}
-			}else{
-				msj = "El enemigo ya existe en este nivel";
-			}
-            
-        }
-        return msj;
-	}
+
 
 	/**
 	 * 
 	 * @param level
 	 * @param Treasure
 	 */
-	public String addTreasureToLevel(String levelid, Treasure newTreasure) {
-		int pos = searchLevelById(levelid);
-		boolean isRepeated;
-        int treasurePos;
-        String msj ="";
-        if (pos==-1){
-            msj = "No se encontro el nivel";
-        }else{
-			isRepeated = canvas.getLevels()[pos].isTreasureRepeated(newTreasure.getName());
-			if (!isRepeated){
-				treasurePos = canvas.getLevels()[pos].TreasureHasEmptyPos();
-            	if(treasurePos ==-1){
-                	msj = "La capacidad de enemigos esta en su limite";
-           		}else{
-                	canvas.getLevels()[pos].getTreasures()[treasurePos]=newTreasure;
-					canvas.getLevels()[pos].calculateDifficulty();
-                	msj = "Tesoro agregado";
 
-            	}
-			}else{
-				msj = "El tesoro ya existe en este nivel";
-			}
-            
-        }
-        return msj;
-	}
 
 	public String calculateTop5() {
 		String scoreboard = "Top 5 jugadores \n";	
@@ -291,24 +413,7 @@ public class VideoGame {
 		return pos;
 	}
 
-	public String getAllTreasures(){
-		int total = 0;
-		String msj ="";
-		for (int i = 0; i<canvas.getLevels().length; i++){
-			total+=canvas.getLevels()[i].countTresuresValue();
-		}
-		return msj = "En total de los niveles hay "+total+" Diamantes";
-	}
-	public String getAllEnemiesType(int type){
-		Type toSearch = giveType(type);
-		int total = 0;
-		String msj;
-		for (int i = 0; i<canvas.getLevels().length; i++){
-			total +=canvas.getLevels()[i].countEnemiesByType(toSearch);
-			
-		}
-		return msj = "En total de los niveles hay "+total+" "+toSearch+"(s)";
-	}
+
 	public Type giveType(int type){
 		Type newType = Type.indefinido;
 		if(type == 1){
@@ -326,93 +431,8 @@ public class VideoGame {
 		return newType;
 	}
 
-	public String getModeTreasure() {
-		String msj = "";
-        ArrayList<int[]> treasureValues = new ArrayList<int[]>();
-        int value = 0;
-		boolean isAdded;
-        int[] values = new int[]{0, 0};
-		treasureValues.add(values);
-		for(int i = 0; i<canvas.getLevels().length; i++){
-			for(int j = 0; j<canvas.getLevels()[0].getTreasures().length; j++){
-				if(canvas.getLevels()[i].getTreasures()[j]!=null){
-					values = new int[]{canvas.getLevels()[i].getTreasures()[j].getValue(), 1};
-					isAdded = true;
-					for(int k = 0; k<treasureValues.size(); k++){
-						if(treasureValues.get(k)[0]==values[0]){
-							treasureValues.get(k)[1]+=1;
-							isAdded = false;
-						}
-					}
-					if (isAdded == true){
-						treasureValues.add(values);	
-					}
-					
-				}
-			}
-		}
-			int[] max = treasureValues.get(0);
-			int[] min;
-			for(int i = 1; i<treasureValues.size(); i++){
-				min = treasureValues.get(i)	;
-				if(min[1]>max[1]){
-					max = min;
-				}
-			}
-        msj ="El tesoro que mas se repite en todos los niveles son los que contienen "+max[0]+" Diamantes \n"+
-		"Total: "+max[1];
-        return msj;
-	}
-	public String getMaxEnemy(){
-		String msj="No se encontraron enemigos";
-		int pos = -1;
-		int enemyPos= -1;
-		int max = 0;
-		int min = 0;
-		for(int i = 0; i<canvas.getLevels().length; i++){
-			for(int j = 0; j<canvas.getLevels()[0].getEnemies().length; j++){
-				
-				if (canvas.getLevels()[i].getEnemies()[j]!=null){
-					min = canvas.getLevels()[i].getEnemies()[j].getLoot();
-					if(min > max){
-						max = min;
-						pos = i;
-						enemyPos = j;
-					}
-				}
-			}
-		}
-		if (enemyPos != -1 && pos != -1){
-			msj = "El enemigo que mas puntos otorga es un "+canvas.getLevels()[pos].getEnemies()[enemyPos].getType()+" en el nivel "+(pos+1)+"\n"+
-			"Puntos en total: "+canvas.getLevels()[pos].getEnemies()[enemyPos].getLoot();
-		}
-		return msj;
-	}
 
-	public String getALLConsonants(){
-		String msj = "";
-		int contador = 0;
-		String enemyName = "";
-		String consonantes ="bcdfghjklmnpqrstvwxyz"; 
-		for(int i = 0; i<canvas.getLevels().length; i++){
-			for(int j = 0; j<canvas.getLevels()[0].getEnemies().length; j++){
-				if (canvas.getLevels()[i].getEnemies()[j]!=null){
-					enemyName = canvas.getLevels()[i].getEnemies()[j].getName();
-					for(int k = 0; k<consonantes.length(); k++){
-						for(int l= 0; l<enemyName.length(); l++){
-							if(enemyName.charAt(l)==consonantes.charAt(k)){
-								contador++;
-							}
-						
-						}
-					}
-				}
-			}
-		
-		
-		}
-		return msj ="En el juego los enemigos tienen un total de "+contador+" consonantes en sus nombres";
-	}
+
 }
 
 
