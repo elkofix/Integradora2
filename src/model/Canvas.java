48e9366c9f35f7119f21bea6176150f5dcc96428
elkofix <holasoygermamwow@gmail.com>
2022-10-02 23:26:18 -0500

    Complete methods javadoc

--- src/model/Canvas.java
+++ src/model/Canvas.java
@@ -6,8 +6,11 @@ public class Canvas {
     private int Ylength;
     private Level[] levels;
 
-
-
+    /**
+     * Constructor of canvas class
+     * @param Xlength Xpixels
+     * @param Ylength Ypixels
+     */
     public Canvas(int Xlength, int Ylength) {
 		this.Xlength = Xlength;
         this.Ylength = Ylength;
@@ -16,16 +19,25 @@ public class Canvas {
             levels[i] = new Level(i+1+"", ((1+i)*10)+(int)(Math.round(Math.random()*6)));
         }
 	}
-
+    /**
+     * gets de Ypixels
+     * @return Ypixels
+     */
     public int getYlength() {
         return Ylength;
     }
-
+    /**
+     * get de Xpixels
+     * @return Xpixels
+     */
     public int getXlength() {
         return Xlength;
     }
 
-    
+    /**
+     * gets the levels array
+     * @return levels array
+     */
     public Level[] getLevels() {
         return levels;
     }
--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -6,11 +6,6 @@ public class Enemy {
 
 	private String name;
     private int attack;
-	public int getAttack() {
-        return attack;
-    }
-
-
     private int loot;
     private double Xpos;
 	private double Ypos;
@@ -26,20 +21,38 @@ public class Enemy {
         Xpos =  (Math.random()*1279);
         Ypos =  (Math.random()*719);
 	}
-
+    /**
+     * gets the points it substracts to the player if its victorious
+     * @return points it substracts to the player if its victorious
+     */
+    public int getAttack() {
+        return attack;
+    }
+    /**
+     * gets the points it adds to the player if its defeated
+     * @return points it adds to the player if its defeated
+     */
     public int getLoot() {
         return loot;
     }
-
+    /**
+     * gets the type of enemy
+     * @return type of enemy
+     */
     public Type getType() {
         return type;
     }
-    
+    /**
+     * gets the name of the enemy
+     * @return name of the enemy
+     */
 	public String getName() {
         return name;
     }
-    	
-
+    /**
+     * sets the type of enemy to a new one
+     * @param type new type of enemy
+     */
     public void setType(Type type) {
         this.type = type;
     }
