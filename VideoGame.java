0a7ab0522c97ecee7e33134553e6fa96f25d8afd
elkofix <holasoygermamwow@gmail.com>
2022-10-02 21:38:13 -0500

    Adding class diagram documentation

---
+++ VideoGame.java
@@ -0,0 +1,85 @@
+public class VideoGame {
+
+	private String name;
+
+	public VideoGame() {
+		// TODO - implement VideoGame.VideoGame
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Player
+	 */
+	public String addPlayer(int Player) {
+		// TODO - implement VideoGame.addPlayer
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Player
+	 */
+	public String upgradePlayer(int Player) {
+		// TODO - implement VideoGame.upgradePlayer
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Level
+	 */
+	public String addLevel(int Level) {
+		// TODO - implement VideoGame.addLevel
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement VideoGame.operation
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param level
+	 * @param Enemy
+	 */
+	public String addEnemyToLevel(int level, int Enemy) {
+		// TODO - implement VideoGame.addEnemyToLevel
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param level
+	 * @param Treasure
+	 */
+	public String addTreasureToLevel(int level, int Treasure) {
+		// TODO - implement VideoGame.addTreasureToLevel
+		throw new UnsupportedOperationException();
+	}
+
+	public Player[] calculateTop5() {
+		// TODO - implement VideoGame.calculateTop5
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nick
+	 */
+	public int searchPlayerbyNick(int nick) {
+		// TODO - implement VideoGame.searchPlayerbyNick
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Level
+	 */
+	public int searchLevelById(int Level) {
+		// TODO - implement VideoGame.searchLevelById
+		throw new UnsupportedOperationException();
+	}
+
+}
